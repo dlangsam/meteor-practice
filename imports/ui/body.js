5ef9d186e79ae26cf278c8b9c3152d5aaa08564a
Devorah Langsam <devorah@langsam.org>
2016-08-30 15:35:13 -0400

    Add security

--- imports/ui/body.js
+++ imports/ui/body.js
@@ -37,14 +37,8 @@ Template.body.events({
     const target = event.target;
     const text = target.text.value;
  
-    // Insert a task into the collection
-    Tasks.insert({
-      text,
-      createdAt: new Date(), // current time
-      owner: Meteor.userId(),
-      username: Meteor.user().username,
-
-    });
+ 	// Insert a task into the collection
+    Meteor.call('tasks.insert', text);
  
     // Clear form
     target.text.value = '';
